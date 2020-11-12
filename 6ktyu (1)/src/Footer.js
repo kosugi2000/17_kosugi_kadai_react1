001df127f0ec4d719f00a068d80b747eefaab97a
kosugi2000 <69379415+kosugi2000@users.noreply.github.com>
2020-11-12 17:47:22 +0900

    Add files via upload

--- 6ktyu (1)/src/Footer.js
+++ 6ktyu (1)/src/Footer.js
@@ -2,13 +2,20 @@ import React from "react";
 import styled from "styled-components";
 import ImgPath from "./img/mainbg.png";
 
+const StyleApps = styled.div`
+  text-align: center;
+  justify-content: space-between;
+  width: 1200px;
+  height: auto;
+  margin: 0 auto;
+`;
 const Footer = () => {
   return (
-    <>
+    <StyleApps>
       <div id="footer">
         <p>copyrights 2016 Cheeese Academy Tokyo All RIghts Reserved.</p>
       </div>
-    </>
+    </StyleApps>
   );
 };
 export default Footer;
